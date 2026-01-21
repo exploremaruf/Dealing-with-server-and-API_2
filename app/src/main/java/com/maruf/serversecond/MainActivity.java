594e144aa53817be306872790489c3a07e0ac8d7
exploremaruf <cyberistmaruf@gmail.com>
2026-01-21 21:56:08 +0600

    WIP

--- app/src/main/java/com/maruf/serversecond/MainActivity.java
+++ app/src/main/java/com/maruf/serversecond/MainActivity.java
@@ -1,6 +1,15 @@
 package com.maruf.serversecond;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +17,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.lang.reflect.Method;
+
 public class MainActivity extends AppCompatActivity {
 
+    ProgressBar progressBar;
+
+    Button btninput;
+    EditText edname, edemail,edadress;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -21,6 +45,63 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btninput=findViewById(R.id.btninput);
+        edname=findViewById(R.id.edname);
+        edemail=findViewById(R.id.edemail);
+        edadress=findViewById(R.id.edadress);
+        progressBar=findViewById(R.id.progressBar);
+
+        btninput.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String name = edname.getText().toString();
+                String email = edemail.getText().toString();
+                String adress = edadress.getText().toString();
+
+                String url = "http://192.168.1.107/Apps/connect.php?n="+name+"&e="+email+"&a="+adress;
+
+                progressBar.setVisibility(VISIBLE);
+
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+                StringRequest stringRequest= new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String s) {
+
+                        progressBar.setVisibility(GONE);
+
+                        Log.d("serverresponse","response is"+s);
+
+
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+
+                    }
+                }
+
+                );
+
+                 requestQueue.add(stringRequest);
+
+
+
+
+
+
+
+
+            }
+        });
+
+
+
     }
 
 }
